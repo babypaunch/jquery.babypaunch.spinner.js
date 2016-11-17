e89143cfd89a7bb3dc3e7aae0294ad43ad057eb1
babypaunch <babypaunch@gmail.com>
2016-11-17 14:19:47 +0900

    js 수정, min.js 추가
    1. json 옵션 keyName 모두 수정
    2. min.js 추가
    3. 샘플 보완

--- jquery.babypaunch.spinner.js
+++ jquery.babypaunch.spinner.js
@@ -1,8 +1,9 @@
 /*
 * 개발: 정대규
 * 최초: 2016.11.15
+* 수정: 2016.11.17
 */
-$.fn.spinner = function(color){
+$.fn.spinner = function(data){
 	/*
 	* body의 scroll을 제어하기 위해
 	* spinner가 show/hide되는 시점에 처리를 위한 이벤트에 callback을 이용한다.
@@ -16,14 +17,13 @@ $.fn.spinner = function(color){
 	});
 
 	//options
-	var $color = {
-		front: "white"
-		, back: "0,0,0"
-		, opacity: "0.5"
-		, content: "\\21BB"
+	var $spinner = {
+		color: "white"
+		, background: "rgba(0,0,0,0.5)"
+		, html: "&#x21BB;"
 	};
 
-	this.init = function($color){
+	this.init = function($spinner){
 		return "<style id='data-spinner-style'>\n"
 			+ "body.unselectable {\n"
 				+ "\t-webkit-user-select: none;"
@@ -34,23 +34,22 @@ $.fn.spinner = function(color){
 			+ "}\n"
 			+ "[data-spinner-layer] {\n"
 				+ "\tdisplay: none; position: fixed; top: 0; left: 0;"
-				+ "\tbackground: rgba(" + $color.back + "," + $color.opacity + ");"
+				+ "\tbackground: " + $spinner.background + ";"
 				+ "\twidth: 100%; height: 100%; padding: 0; margin: 0;\n"
 			+ "}\n"
 			+ "[data-spinner-bar] {\n"
-				+ "\tcolor: " + $color.front + ";"
+				+ "\tcolor: " + $spinner.color + ";"
 				+ "\tposition: absolute; top: calc(50% - 30px); left: calc(50% - 15px); font-weight: bold; font-size: 40px;"
 				+ "\t-webkit-animation: data-spinner 2s linear infinite; -moz-animation: data-spinner 2s linear infinite; animation: data-spinner 2s linear infinite;\n"
 			+ "}\n"
-			+ "[data-spinner-bar]:before {content: '" + $color.content + "';}\n"
 			+ "\t@-moz-keyframes data-spinner {100% {-moz-transform: rotate(360deg); transform: rotate(360deg);}}\n"
 			+ "\t@-webkit-keyframes data-spinner {100% {-webkit-transform: rotate(360deg); transform: rotate(360deg);}}\n"
 			+ "\t@keyframes data-spinner {100% {-webkit-transform: rotate(360deg); transform: rotate(360deg);}}\n"
 		+ "</style>";
 	}
 
-	$.extend($color, color);
-	var $style = this.init($color);
+	$.extend($spinner, data);
+	var $style = this.init($spinner);
 
 	return this.each(function(){
 		if($("#data-spinner-style").length === 0){
@@ -60,7 +59,7 @@ $.fn.spinner = function(color){
 		$spin = $(this);
 		$spin
 			.attr({"data-spinner-layer": "", "data-spinner-body": $("body").css("overflow")})
-			.append("<div data-spinner-bar></div>")
+			.append("<div data-spinner-bar>" + $spinner.html + "</div>")
 			.on("show", function(){
 				$("body").css("overflow", "hidden");
 			})
